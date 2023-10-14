277b03f9697129241e256cb729b8dc4ee2638f19
theneek14 <neel.19je0558@mc.iitism.ac.in>
2023-10-14 19:42:08 +0530

    Update BoardHeader.jsx

--- src/components/Board/BoardHeader.jsx
+++ src/components/Board/BoardHeader.jsx
@@ -1,22 +1,22 @@
 import "./Board.css";
-import { AiOutlinePlus } from "react-icons/ai";
-import { BiDotsHorizontalRounded } from "react-icons/bi";
-import { SiInstatus } from "react-icons/si";
+import { BiPlusCircle } from "react-icons/bi";
+import { BiDotsHorizontal } from "react-icons/bi";
+import { SiQuasar } from "react-icons/si";
 
 const BoardHeader = ({tickets, header}) => {
     return (
         <div className="board-header">
             <div>
                 <div className="flex-gap">
-                    <SiInstatus className="bg-color-status-icon" />
+                    <SiQuasar className="bg-color-status-icon" />
                     <p>{header}</p>
                     <span>{tickets.length}</span>
                 </div>
             </div>
 
             <div className="flex-gap">
-                <AiOutlinePlus className="bg-color-icon" />
-                <BiDotsHorizontalRounded className="bg-color-icon" />
+                <BiPlusCircle className="bg-color-icon" />
+                <BiDotsHorizontal className="bg-color-icon" />
             </div>
         </div>
     );
